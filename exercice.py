0b0a819b3cf6c2aa776cae3929902d28995bba87
luckyNui <48302583+luckyNui@users.noreply.github.com>
2021-01-26 14:15:50 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -27,11 +27,12 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+
+    return round((temperature-32)*5/9)
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature*1.8 +32
 
 
 def main() -> None:
